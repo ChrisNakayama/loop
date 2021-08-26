4909251c9a996e762a36b0b6093ace36ed67fe40
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-08-26 00:27:58 -0700

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,24 @@
+function beep(numberInput) {
+  var numberArray + [];
+  for (i=0; i <= numberInput; i++) {
+    if (i.toString().includes(21)) {
+      numberArray.push("<li>Boop.</li>");
+    } else if (i.toString().includes(13)) {
+      numberArray.push("<li>Won't you be my neighbor?</li>");
+    } else if (i.toString().includes(32)) {
+      numberArray.push("<li>Won't you be my neighbor?</li>");
+    } else if (i.toString().includes(1)) {
+      numberArray.push("<li>Beep!</li>");
+    } else if (i.toString().includes(2)) {
+      numberArray.push("<li>Boop!</li>");
+    } else if (i.tostring().includes(3)) {
+      numberArray.push("<li>Won't you be my neighbor?</li>");
+    } else {
+      numberArray.push("<li>" + i + "</li>");   
+    }
+  } 
+  return numberArray;
+};
 
 
-
-function rangeOfNumbers(start,end) {
-  let rangeArray = [];
-  for (let i = start; i <= end; i++) {
-    rangeArray.push(i + "");
-  }
-  return rangeArray.map(x => x.includes("2") ? "Boop!" : x.includes("1") ? "Beep" : x);
-
-}
-console.log(rangeOfNumbers(0,12).join());
-
-function rangeOfNumbersFour(start,end) {
-  let rangeArray = [];
-  for (let i = start; i <= end; i++) {
-    rangeArray.push(i + "");
-  }
-  return rangeArray.map(x => x.includes("3") ? "Wont you be my Neighbor?" : x.includes("21") ? "Boop" : x);
-
-}
-console.log(rangeOfNumbers(0,12).join());
-
-function rangeOfNumbersFive(start,end) {
-  let rangeArray = [];
-  for (let i = start; i <= end; i++) {
-    rangeArray.push(i + "");
-  }
-  return rangeArray.map(x => x.includes("13") ? "Won't you be my Neighbor?" : x.includes("32") ? "Won't you be my Neighbor?" : x);
-
-}
-console.log(rangeOfNumbers(0,12).join());
-
-
-
+""
