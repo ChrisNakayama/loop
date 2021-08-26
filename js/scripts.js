886722679b773712f60e74b5f53497e4205a7a9d
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-08-26 12:06:42 -0700

    fix errors in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,11 @@
 function beep(numberInput) {
   var numberArray = [];
   for (i=0; i <= numberInput; i++) {
-    if (i.toString().includes(32)) {
-      numberArray.push("<li>Won't you be my neighbor?</li>");
-    } else if (i.toString().includes(21)) {
-      numberArray.push("<li>Boop.</li>");
-    } else if (i.toString().includes(13)) {
-      numberArray.push("<li>Won't you be my neighbor?</li>");
-    } else if (i.toString().includes(3)) {
-      numberArray.push("<li>Won't you be my neighbor?</li>");
+    if (i.toString().includes(3)) {
+    numberArray.push("<li>Won't you be my neighbor?</li>");
     } else if (i.toString().includes(2)) {
       numberArray.push("<li>Boop!</li>");
-    } else if (i.tostring().includes(1)) {
+    } else if (i.toString().includes(1)) {
       numberArray.push("<li>Beep!</li>");
     } else {
       numberArray.push("<li>" + i + "</li>");   
