64b9d5b55329b9fb3baa9ce356acd8bdfecd5f2a
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-08-26 01:21:42 -0700

    added JS and UI rough

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 function beep(numberInput) {
-  var numberArray + [];
+  var numberArray = [];
   for (i=0; i <= numberInput; i++) {
-    if (i.toString().includes(21)) {
+    if (i.toString().includes(32)) {
+      numberArray.push("<li>Won't you be my neighbor?</li>");
+    } else if (i.toString().includes(21)) {
       numberArray.push("<li>Boop.</li>");
     } else if (i.toString().includes(13)) {
       numberArray.push("<li>Won't you be my neighbor?</li>");
-    } else if (i.toString().includes(32)) {
+    } else if (i.toString().includes(3)) {
       numberArray.push("<li>Won't you be my neighbor?</li>");
-    } else if (i.toString().includes(1)) {
-      numberArray.push("<li>Beep!</li>");
     } else if (i.toString().includes(2)) {
       numberArray.push("<li>Boop!</li>");
-    } else if (i.tostring().includes(3)) {
-      numberArray.push("<li>Won't you be my neighbor?</li>");
+    } else if (i.tostring().includes(1)) {
+      numberArray.push("<li>Beep!</li>");
     } else {
       numberArray.push("<li>" + i + "</li>");   
     }
@@ -20,5 +20,20 @@ function beep(numberInput) {
   return numberArray;
 };
 
+$(document).ready(function() {
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+    var numberInput = parseInt($("input#number").val());
+    let result = beep(numberInput);
+    $(".unstyled").text("");
+    
+    result.forEach(outputList);
 
-""
+    function outputList(result) {
+      $(".unstyled").append(result);
+    };
+    
+    $("output").show(): this
+
+  });
+});    
