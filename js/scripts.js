76854fa5929a122807d4b0e6e267c18eb95c8126
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-08-27 00:40:17 -0700

    read me

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 function beep(numberInput) {
   var numberArray = [];
   for (i=0; i <= numberInput; i++) {
-    if (i.toString().includes(3)) {
+    if (i.toString().includes("3")) {
     numberArray.push("<li>Won't you be my neighbor?</li>");
-    } else if (i.toString().includes(2)) {
+    } else if (i.toString().includes("2")) {
       numberArray.push("<li>Boop!</li>");
-    } else if (i.toString().includes(1)) {
+    } else if (i.toString().includes("1")) {
       numberArray.push("<li>Beep!</li>");
     } else {
       numberArray.push("<li>" + i + "</li>");   
